9dffa3c10fef5ffafc4fdc890052173aaa07d097
sharkdad <sharkdad@users.noreply.github.com>
2020-09-13 16:18:44 -0700

    Load more UI button

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,52 +1,58 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, memo } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-const Link = ({ link }) => <>
-  <div className="container-fluid d-flex flex-column align-items-center">
+function callAsync(func: () => Promise<any>): void {
+  func().catch(e => console.error(e))
+}
+
+var LinkList = ({ links }) => <>
+  {links.map(link => <div key={link.id}>
     {link.embed && <div dangerouslySetInnerHTML={{ __html: link.embed }} />}
-  </div>
-  <div className="container d-flex flex-column align-items-center">
-    <b><a rel="noopener noreferrer" target="_blank" href={link.url}>{link.title}</a></b>
-    <div><small>{link.subreddit} - <a rel="noopener noreferrer" target="_blank" href={`https://old.reddit.com${link.permalink}`}>{link.num_comments} comments</a> - {link.posted_at} - {link.score}</small></div>
-  </div>
+    <div className="container d-flex flex-column align-items-center">
+      <b><a rel="noopener noreferrer" target="_blank" href={link.url}>{link.title}</a></b>
+      <div><small>
+        {link.subreddit}
+      - <a rel="noopener noreferrer" target="_blank"
+          href={`https://old.reddit.com${link.permalink}`}>{link.num_comments} comments</a>
+      - {link.posted_at}
+      - {link.score}
+      </small></div>
+    </div>
+  </div>)}
 </>
+LinkList = memo(LinkList)
 
-function LinkList() {
-  const [result, setResult] = useState({} as any)
+function LinkLoader() {
+  const [numLoads, setNumLoads] = useState(0)
+  const [results, setResults] = useState([] as any[])
 
-  async function getLinks(): Promise<void> {
-    try {
-      const response = await fetch("/svc/api/links/?is_read=false&ordering=-score")
-      if (!response.ok) {
-        throw new Error(`Response not ok, status:${response.status} statusText:${response.statusText}`)
-      }
-      setResult(await response.json())
-    } catch (e) {
-      console.error(e)
+  useEffect(() => callAsync(async () => {
+    const response = await fetch("/svc/api/links/?is_read=false&ordering=-score")
+    if (!response.ok) {
+      throw new Error(`Response not ok, status:${response.status} statusText:${response.statusText}`)
     }
-  }
-
-  useEffect(() => {
-    getLinks()
-  }, [])
-
-  const links = result.results || []
+    const result = await response.json()
+    setResults(r => [...r, result])
+  }), [numLoads])
 
   return <>
-    {links.map(link => <Link link={link} />)}
+    {results.map((result, index) => <LinkList links={result.results} key={index} />)}
+    <button type="button" className="btn btn-primary" onClick={() => setNumLoads(n => n + 1)}>Load more</button>
   </>
 }
 
 ReactDOM.render(
   <React.StrictMode>
-    <div className="container">
-      <h1>noscroll</h1>
+    <div className="container-fluid d-flex flex-column align-items-center">
+      <div className="container d-flex flex-column align-items-center">
+        <h1>noscroll</h1>
+      </div>
+      <LinkLoader />
     </div>
-    <LinkList />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
