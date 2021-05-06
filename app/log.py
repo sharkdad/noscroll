c5d3d74b33b40023e6b2372d8bcd3cb8bc4f61f4
sharkdad <46014430+sharkdad@users.noreply.github.com>
2021-05-05 17:03:24 -0700

    Access logging

--- app/log.py
+++ app/log.py
@@ -23,13 +23,37 @@ BAD_LOG_RECORD_ATTRS = set(
 )
 
 
-class JSONFormatter(logging.Formatter):
+class UTCFormatter(logging.Formatter):
     converter = time.gmtime
     default_time_format = "%Y-%m-%dT%H:%M:%S"
     default_msec_format = "%s.%03dZ"
 
+
+class JSONFormatter(UTCFormatter):
     def format(self, record):
         record.message = super().format(record)
         data = {k: v for k, v in vars(record).items() if k not in BAD_LOG_RECORD_ATTRS}
         data["asctime"] = self.formatTime(record)
         return json.dumps(data, default=str)
+
+
+class GunicornAccessLogJSONFormatter(UTCFormatter):
+    converter = time.gmtime
+    default_time_format = "%Y-%m-%dT%H:%M:%S"
+    default_msec_format = "%s.%03dZ"
+
+    def format(self, record):
+        message = super().format(record)
+        return json.dumps(
+            dict(
+                name=record.name,
+                levelname=record.levelname,
+                asctime=self.formatTime(record),
+                message=message,
+                method=record.args["m"],
+                path=record.args["U"],
+                status=record.args["s"],
+                ua=record.args["a"],
+                time_ms=record.args["M"],
+            )
+        )
--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,6 +2,8 @@ wsgi_app = "project.wsgi"
 bind = ["127.0.0.1:8001"]
 threads = 10
 
+access_log_format = "%(m)s %(U)s %(s)s"
+
 logconfig_dict = {
     "version": 1,
     "disable_existing_loggers": False,
@@ -10,11 +12,19 @@ logconfig_dict = {
             "()": "app.log.JSONFormatter",
             "format": "%(message)s",
         },
+        "json_access": {
+            "()": "app.log.GunicornAccessLogJSONFormatter",
+            "format": "%(message)s",
+        },
     },
     "handlers": {
         "console": {
             "class": "logging.StreamHandler",
             "formatter": "json",
+        },
+        "console_access": {
+            "class": "logging.StreamHandler",
+            "formatter": "json_access",
         },
     },
     "loggers": {
@@ -23,7 +33,7 @@ logconfig_dict = {
             "propagate": True,
         },
         "gunicorn.access": {
-            "handlers": [],
+            "handlers": ["console_access"],
             "propagate": False,
         },
     },
